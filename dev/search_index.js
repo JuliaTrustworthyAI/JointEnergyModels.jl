459c46e5dce4b1361b7e6f9afe9d004703b766a3
Documenter.jl <documenter@juliadocs.github.io>
2023-03-01 10:36:36 +0000

    build based on eeb8c90

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JointEnergyModels","category":"page"},{"location":"#JointEnergyModels","page":"Home","title":"JointEnergyModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JointEnergyModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JointEnergyModels]","category":"page"}]
+}

f290b80fee75675e62a5ffcdbdc54208b732b17c
pat-alt <altmeyerpat@gmail.com>
2023-03-06 15:34:18 +0100

    some core things

--- src/Samplers.jl
+++ src/Samplers.jl
@@ -6,19 +6,54 @@ using Flux.Optimise: apply!
 using ..JointEnergyModels
 using ..JointEnergyModels: AbstractSampler
 
+@doc raw"""
+    ConditionalSampler <: AbstractSampler
+
+Generates conditional samples: $x \sim p(x|y).$
+"""
 struct ConditionalSampler <: AbstractSampler
-    𝒟x::Distribution
-    𝒟y::Distribution
+    𝒟ₓ::Distribution
+    y::Union{Int,Distribution}
     niter::Int
 end
-ConditionalSampler(𝒟x::Distribution, 𝒟y::Distribution, niter::Int) = ConditionalSampler(𝒟x, 𝒟y, niter)
+ConditionalSampler(𝒟ₓ::Distribution, y::Union{Int,Distribution}, niter::Int) = ConditionalSampler(𝒟ₓ, y, niter)
 
 function (sampler::ConditionalSampler)(model, rule::JointEnergyModels.Optimiser, dims::Dims)
 
     # Setup:
-    x = rand(d, dims)
-    y = rand(𝒟y)
+    x = rand(sampler.𝒟ₓ, dims)
+    if typeof(y) <: Distribution
+        y = rand(sampler.y)
+    end
     f(x) = energy(model, x, y)
+
+    # Training:
+    for i in 1:sampler.niter
+        Δ = gradient(f, x)[1]
+        Δ = apply!(rule, x, Δ)
+        x -= Δ
+    end
+
+    return x
+
+end
+
+@doc raw"""
+    UnonditionalSampler <: AbstractSampler
+
+Generates unconditional samples: $x \sim p(x).$
+"""
+struct UnconditionalSampler <: AbstractSampler
+    𝒟ₓ::Distribution
+    niter::Int
+end
+UnconditionalSampler(𝒟ₓ::Distribution, niter::Int) = UnconditionalSampler(𝒟ₓ, niter)
+
+function (sampler::UnconditionalSampler)(model, rule::JointEnergyModels.Optimiser, dims::Dims)
+
+    # Setup:
+    x = rand(sampler.𝒟ₓ, dims)
+    f(x) = energy(model, x)
 
     # Training:
     for i in 1:sampler.niter

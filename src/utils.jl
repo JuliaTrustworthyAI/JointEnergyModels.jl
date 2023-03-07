fa24ffcf5a12682985d62391d9f60959ad2f5c8c
pat-alt <altmeyerpat@gmail.com>
2023-03-07 14:57:21 +0100

    queee

--- src/utils.jl
+++ src/utils.jl
@@ -5,7 +5,11 @@ using StatsBase
 
 Computes the energy for unconditional samples $x \sim p_{\theta}(x)$: $E(x)=-\text{LogSumExp}_y f_{\theta}(x)[y]$.
 """
-energy(f, x; agg=mean) = agg(map(x -> -logsumexp(f(x)), eachslice(x, dims=ndims(x))))
+function energy(f, x; agg=mean)
+    ŷ = f(x)
+    E = agg(map(y -> -logsumexp(y), eachslice(ŷ, dims=ndims(ŷ))))
+    return E
+end
 
 @doc raw"""
     energy(f, x, y::Int; agg=mean)
@@ -13,7 +17,8 @@ energy(f, x; agg=mean) = agg(map(x -> -logsumexp(f(x)), eachslice(x, dims=ndims(
 Computes the energy for conditional samples $x \sim p_{\theta}(x|y)$: $E(x)=- f_{\theta}(x)[y]$.
 """
 function energy(f, x, y::Int; agg=mean)
-    agg(map(x -> -f(x)[y], eachslice(x, dims=ndims(x))))
+    ŷ = f(x)
+    agg(map(_y -> -_y[y], eachslice(ŷ, dims=ndims(ŷ))))
 end
 
 

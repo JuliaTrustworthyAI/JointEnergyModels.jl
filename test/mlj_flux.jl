90d17cf500a6951d2be1f85f174bf5ded567d870
Pat Alt <55311242+pat-alt@users.noreply.github.com>
2023-09-16 11:37:39 +0200

    test gpu

--- test/mlj_flux.jl
+++ test/mlj_flux.jl
@@ -1,28 +1,28 @@
-clf = JointEnergyClassifier(
-    sampler;
-    builder=MLJFlux.MLP(hidden=(32, 32, 32,), σ=Flux.relu),
-    batch_size=batch_size,
-    finaliser=x -> x,
-    loss=Flux.Losses.logitcrossentropy,
-    jem_training_params=(α=[1.0, 1.0, 0.1], verbosity=5,)
-)
-mach = machine(clf, X, y)
-
 @testset "JointEnergyClassifier" begin
-    @testset "constructor" begin
-        @testset "default" begin
-            @test clf isa MLJFlux.MLJFluxModel
-            @test clf isa JointEnergyClassifier
-        end
-    end
+    @testset "CPU" begin
+        clf = JointEnergyClassifier(
+            sampler;
+            builder=MLJFlux.MLP(hidden=(32, 32, 32,), σ=Flux.relu),
+            batch_size=batch_size,
+            finaliser=x -> x,
+            loss=Flux.Losses.logitcrossentropy,
+            jem_training_params=(α=[1.0, 1.0, 0.1], verbosity=5,)
+        )
+        mach = machine(clf, X, y)
 
-    @testset "fit" begin
-        @testset "default" begin
+        @testset "constructor" begin
+            @testset "default" begin
+                @test clf isa MLJFlux.MLJFluxModel
+                @test clf isa JointEnergyClassifier
+            end
+        end
+
+        @testset "fitting" begin
             fit!(mach)
         end
     end
 
-    @testset "gpu" begin
+    @testset "GPU" begin
         clf = JointEnergyClassifier(
             sampler;
             builder=MLJFlux.MLP(hidden=(32, 32, 32,), σ=Flux.relu),
@@ -33,5 +33,16 @@ mach = machine(clf, X, y)
             acceleration = CUDALibs()
         )
         mach = machine(clf, X, y)
+        
+        @testset "constructor" begin
+            @testset "default" begin
+                @test clf isa MLJFlux.MLJFluxModel
+                @test clf isa JointEnergyClassifier
+            end
+        end
+
+        @testset "fitting" begin
+            fit!(mach)
+        end
     end
 end
